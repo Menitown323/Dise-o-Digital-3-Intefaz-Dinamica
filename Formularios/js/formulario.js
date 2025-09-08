ba0c2cd5943106918fdabd96a7bb0238ab8e8d1a
Manuel <Manuel@gmail.com>
2025-09-08 12:40:32 -0600

    Se agrego una funcion para agregar imagenes al formulario

--- Formularios/js/formulario.js
+++ Formularios/js/formulario.js
@@ -1,7 +1,10 @@
 const formulario_producto_nuevo = document.querySelector("#formulario_producto_nuevo");
+let productos = [];
+let src_imagen_producto = "";
 
 class Producto{
-    constructor(id, nombre, descripcion, precio){
+    constructor(id, imagen, nombre, descripcion, precio){
+        this.Imagen = imagen;
         this.Id = id;
         this.Nombre = nombre;
         this.Descripcion = descripcion;
@@ -9,6 +12,7 @@ class Producto{
     }
     obtenerDatos(){
         console.log(this.Id);
+        console.log(this.Imagen);
         console.log(this.Nombre);
         console.log(this.Descripcion);
         console.log(this.Precio);
@@ -19,7 +23,28 @@ function AgregarProducto(event){
     let lectorFormulario = new FormData(formulario_producto_nuevo);
     //console.log(lectorFormulario)
     const datos = Object.fromEntries(lectorFormulario.entries());
-    const json = JSON.stringify(datos);
-    console.log(json);
+    console.log(datos);
+    if(datos.Nombre = "" && datos. Descripcion != "" && datos.Precio != null) {
+    productos.push(new Producto(productos.length+1,src_imagen_producto,datos.Nombre,datos.Descripcion,datos.Precio));
+    productos.forEach(producto => {
+        producto.ObtenerDatos();
+    });
+    }
+    //const json = JSON.stringify(datos);
+   // console.log(json);
 
+}
+
+function obtenerImagen(event){
+    const file = event.target.files[0];
+    console.log(file);
+    if(file.type === "image/jpeg" || file.type === "image/png"){
+        console.log(file.name);
+        const lector = new FileReader();
+        lector.onload = (event) =>{
+            src_imagen_producto = event.target.result;
+            document.querySelector("#imagen-file").src = event.target.result;
+        }
+        lector.readAsDataURL(file);
+    }
 }
