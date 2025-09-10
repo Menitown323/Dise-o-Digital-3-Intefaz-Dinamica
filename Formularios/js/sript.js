1c2024723911c2545dce41fdc947d5a43a10dfe6
Manuel <Manuel@gmail.com>
2025-09-10 13:16:21 -0600

    Se corrigio el js

--- Formularios/js/sript.js
+++ Formularios/js/sript.js
@@ -4,13 +4,19 @@ function crearimagen(event){
     console.log(event.target.value);
     elementos.innerHTML="";
     for(let i = 0; i< event.target.value; i++){
+    const contenedor = document.createElement("div");
+    contenedor.classList.add("contenedor");
+
     const imagen = new Image();
     imagen.src = "../img/picahu.jpg";
     imagen.classList.add("imagen-nueva");
+
     const titulo = document.createElement("h2");
     titulo.textContent = imagen.src.substring(imagen.src.lastIndexOf('/')+1);
+
     elementos.appendChild(imagen);
     elementos.appendChild(titulo);
+    
     }
     /*const imagen = '<img src="img/picahu.jpg" class="imagen-nueva">'
     elementos.innerHTML = imagen*/
